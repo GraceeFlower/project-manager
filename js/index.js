5b9c9eef54866998f17cc5c7bb06cc3f602252a7
GraceeFlower <1491644103@qq.com>
2020-01-08 23:56:39 +0800

    [WangMin] fix: create a function to calculate the percentage of all the three task card

--- js/index.js
+++ js/index.js
@@ -1,9 +1,20 @@
 function splitThousandsOfBits() {
   let numList = document.getElementsByClassName("specific-figure");
   const regexp = /\d{1,3}(?=(\d{3})+$)/g;
-  for(let item = 0; item < numList.length; item++) {
-    numList[item].innerHTML = numList[item].innerHTML.replace(regexp, '$&,');
-  }
+  [...numList].forEach(item => 
+    item.innerHTML = item.innerHTML.replace(regexp, '$&,'));
 }
 
 splitThousandsOfBits();
+
+function calculatePercent() {
+  let numList = document.getElementsByClassName("specific-figure");
+  let percentList = document.getElementsByClassName("percent-figure");
+  for(let item = 1; item < numList.length; item++) {
+    let percent = Math.round(Number(numList[item].innerHTML.match(/\d/g).join(''))
+       * 100 / Number(numList[0].innerHTML.match(/\d/g).join('')));
+    percentList[item -1].innerHTML = percent + '%';
+  }
+}
+
+calculatePercent();
