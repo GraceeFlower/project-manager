380cf29188bb28928490e92b59c6fb0662beff70
GraceeFlower <1491644103@qq.com>
2020-01-09 17:40:41 +0800

    [WangMin] fix-1: make the data dynamic in the task cards

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
-function calculatePercent(numList, percentList) {
+let numList = document.getElementsByClassName("specific-figure");
+let percentList = document.getElementsByClassName("percent-figure");
+
+function calculatePercent() {
   for(let item = 1; item < numList.length; item++) {
     let percent = Math.round(Number(numList[item].innerHTML)
        * 100 / Number(numList[0].innerHTML));
@@ -6,50 +9,51 @@ function calculatePercent(numList, percentList) {
   }
 }
 
-function splitThousandsOfBits(numList) {
+function splitThousandsOfBits() {
   const regexp = /\d{1,3}(?=(\d{3})+$)/g;
   [...numList].forEach(item => 
     item.innerHTML = item.innerHTML.replace(regexp, '$&,'));
 }
 
-function initStatistic() {
-  let numList = document.getElementsByClassName("specific-figure");
-  let percentList = document.getElementsByClassName("percent-figure");
-  calculatePercent(numList, percentList);
-  splitThousandsOfBits(numList);
-  getListData();
-}
-
 const API_ROOT = "http://localhost:3000/projects";
 let itemList = document.getElementById('item-list');
   
-function getListData() {
+(function getListData() {
   ajax({
     url: API_ROOT,
     method: "GET",
-    success: function (result) {renderItemList(result)},
+    success: function (result) {getItemData(result)},
     fail: function (error) {console.log(error)}
   });
-}
+})();
 
 function markItemStatus(status) {
+  numList[0].innerHTML = Number(numList[0].innerHTML) + 1;
   switch(status) {
     case 'ACTIVE':
+      numList[1].innerHTML = Number(numList[1].innerHTML) + 1;
       return 'active-item';
     case 'PENDING':
+      numList[2].innerHTML = Number(numList[2].innerHTML) + 1;
       return 'pending-item';
     case 'CLOSED':
+      numList[3].innerHTML = Number(numList[3].innerHTML) + 1;
       return 'closed-item';
     default:
       break;
   }
 }
 
-function renderItemList(data) {
+function getItemData(data) {
+  renderItem(data);
+  calculatePercent();
+  splitThousandsOfBits();
+}
+
+function renderItem(data) {
   if (!Array.isArray(data) && !data instanceof Array) {
     return false;
   }
-
   itemList.innerHTML = data.reduce((acc, cur) => {
     const statusStyle = markItemStatus(cur.status);
     return acc += 
@@ -67,5 +71,3 @@ function renderItemList(data) {
     </li>`;
   }, '');
 }
-
-initStatistic();
