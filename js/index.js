219a1edb4c7e6ef51b75a45c341a4b0d0d1973a7
GraceeFlower <1491644103@qq.com>
2020-01-09 10:19:14 +0800

    [WangMin] refactor-1: refact the data initialization function about the project cards

--- js/index.js
+++ js/index.js
@@ -1,20 +1,22 @@
-function splitThousandsOfBits() {
-  let numList = document.getElementsByClassName("specific-figure");
+function calculatePercent(numList, percentList) {
+  for(let item = 1; item < numList.length; item++) {
+    let percent = Math.round(Number(numList[item].innerHTML)
+       * 100 / Number(numList[0].innerHTML));
+    percentList[item -1].innerHTML = percent + '%';
+  }
+}
+
+function splitThousandsOfBits(numList) {
   const regexp = /\d{1,3}(?=(\d{3})+$)/g;
   [...numList].forEach(item => 
     item.innerHTML = item.innerHTML.replace(regexp, '$&,'));
 }
 
-splitThousandsOfBits();
-
-function calculatePercent() {
+function initStatistic() {
   let numList = document.getElementsByClassName("specific-figure");
   let percentList = document.getElementsByClassName("percent-figure");
-  for(let item = 1; item < numList.length; item++) {
-    let percent = Math.round(Number(numList[item].innerHTML.match(/\d/g).join(''))
-       * 100 / Number(numList[0].innerHTML.match(/\d/g).join('')));
-    percentList[item -1].innerHTML = percent + '%';
-  }
+  calculatePercent(numList, percentList);
+  splitThousandsOfBits(numList);
 }
 
-calculatePercent();
+initStatistic();
